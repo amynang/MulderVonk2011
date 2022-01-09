576773999d0e5059be8337aafdbd8a20d70bbfc7
Angelos Amyntas <61004068+amynang@users.noreply.github.com>
2022-01-09 20:07:10 +0100

    progress
    a function that draws samples from a lognormal as you would expect it to, and possible truncation at desired percentile interval.

--- Mulder&Vonk2011.R
+++ Mulder&Vonk2011.R
@@ -52,3 +52,54 @@ masses = fauna %>% group_by(TAX.MORPHON) %>%
   summarise(AvgMass = mean(Mass),
             StDevMass = sd(Mass),
             N = n())
+masses = as.data.frame(masses)
+
+
+m <- 26.4783
+s <- 3.821168e+01
+location <- log(m^2 / sqrt(s^2 + m^2))
+shape <- sqrt(log(1 + (s^2 / m^2)))
+hist(rlnormTrunc(10000, 
+                 meanlog = location, 
+                 sdlog = shape, 
+                 min = qlnorm(.05, 
+                              meanlog = location, 
+                              sdlog = shape), 
+                 max = qlnorm(.95, 
+                              meanlog = location, 
+                              sdlog = shape)),
+     breaks = 1000)
+
+
+rlnormtrunc.intuitive = function(n, m, s, p=.9) {
+  trnc <- EnvStats::rlnormTrunc(n, 
+                     meanlog = log(m^2 / sqrt(s^2 + m^2)), 
+                     sdlog = sqrt(log(1 + (s^2 / m^2))), 
+                     min = qlnorm((1-p)/2, 
+                                  meanlog = log(m^2 / sqrt(s^2 + m^2)), 
+                                  sdlog = sqrt(log(1 + (s^2 / m^2)))), 
+                     max = qlnorm(1-(1-p)/2, 
+                                  meanlog = log(m^2 / sqrt(s^2 + m^2)), 
+                                  sdlog = sqrt(log(1 + (s^2 / m^2)))))
+ return(trnc)
+}
+
+
+hist(rlnormtrunc.intuitive(100000, 26.4783, 3.821168e+01),
+     breaks = 1000)
+plot(density(rlnormtrunc.intuitive(100000, 26.4783, 3.821168e+01)))
+
+
+par(mfrow=c(46,46))
+for (i in 1:91) {
+  try(hist(rlnormtrunc.intuitive(100000, 
+                                 masses[i,2], 
+                                 masses[i,3],
+                                 .98),
+           main = paste("Histogram of" , masses[i,1]),
+           xlab = NULL,
+           ylab = NULL,
+           breaks = 1000))
+}
+
+
