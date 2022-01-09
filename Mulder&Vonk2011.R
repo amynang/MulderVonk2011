2a40835dcb2d7640601058ff9a76b8f88806ab42
Angelos Amyntas <61004068+amynang@users.noreply.github.com>
2022-01-09 18:30:11 +0100

    Update Mulder&Vonk2011.R

--- Mulder&Vonk2011.R
+++ Mulder&Vonk2011.R
@@ -1,4 +1,6 @@
+library(tidyverse)
 library(ggplot2)
+
 
 # get the data
 fauna = read.table("https://figshare.com/ndownloader/files/5620305", 
@@ -9,11 +11,11 @@ dutchagroecosystems = read.table("https://figshare.com/ndownloader/files/5620302
                                  header = T,
                                  sep = "\t")[1:200,1:20]
 
-muldervonk = read.table("H:/Literature/Mulder&Vonk2011_Traitsoilnematofauna.txt",
-                        sep = "\t", header = TRUE)
-muldervonk = muldervonk[1:29552,]
+# muldervonk = read.table("H:/Literature/Mulder&Vonk2011_Traitsoilnematofauna.txt",
+#                         sep = "\t", header = TRUE)
+# muldervonk = muldervonk[1:29552,]
 
-p <- ggplot(muldervonk[muldervonk$Lifestage == "juveniles",], aes(x=Length, color = TAX.MORPHON)) + 
+p <- ggplot(fauna[fauna$Lifestage == "juveniles",], aes(x=Mass, color = TAX.MORPHON)) + 
   geom_density() + 
   facet_wrap(vars(TAX.MORPHON), scales = "free") +
   theme(legend.position="none",
@@ -21,7 +23,7 @@ p <- ggplot(muldervonk[muldervonk$Lifestage == "juveniles",], aes(x=Length, colo
         axis.text.y=element_blank())
 p
 
-p <- ggplot(muldervonk[muldervonk$Lifestage == "female",], aes(x=Length, color = TAX.MORPHON)) + 
+p <- ggplot(fauna[fauna$Lifestage == "female",], aes(x=Mass, color = TAX.MORPHON)) + 
   geom_density() + 
   facet_wrap(vars(TAX.MORPHON), scales = "free") +
   theme(legend.position="none",
@@ -29,10 +31,24 @@ p <- ggplot(muldervonk[muldervonk$Lifestage == "female",], aes(x=Length, color =
         axis.text.y=element_blank())
 p
 
-p <- ggplot(muldervonk[muldervonk$Lifestage == "male",], aes(x=Length, color = TAX.MORPHON)) + 
+p <- ggplot(fauna[fauna$Lifestage == "male",], aes(x=Mass, color = TAX.MORPHON)) + 
   geom_density() + 
   facet_wrap(vars(TAX.MORPHON), scales = "free") +
   theme(legend.position="none",
         axis.text.x=element_blank(),
         axis.text.y=element_blank())
 p
+
+p <- ggplot(fauna, aes(x=Mass, color = TAX.MORPHON)) + 
+  geom_density() + 
+  facet_wrap(vars(TAX.MORPHON), scales = "free") +
+  theme(legend.position="none",
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank())
+p
+
+
+masses = fauna %>% group_by(TAX.MORPHON) %>% 
+  summarise(AvgMass = mean(Mass),
+            StDevMass = sd(Mass),
+            N = n())
